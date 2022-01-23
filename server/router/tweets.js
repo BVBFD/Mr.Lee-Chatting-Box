39158720e55797da1fdc41073b3a0bcd92dc8248
BVBFD <lsevina126@gmail.com>
2022-01-23 15:05:54 +0900

    Code Validator for tweets' text

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,16 +1,23 @@
 import express from "express";
 import "express-async-errors";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.js";
 import * as tweetController from "../controller/tweet.js";
 
 const tweetsRouter = express.Router();
+
+const validator = [
+  body("text").trim().isLength({ min: 3 }).withMessage("3글자 이상 입력하세요"),
+  validate,
+];
 
 tweetsRouter.get("/", tweetController.getAllTweets);
 
 tweetsRouter.get("/:id", tweetController.getTweetsById);
 
-tweetsRouter.post("/", tweetController.createTweet);
+tweetsRouter.post("/", validator, tweetController.createTweet);
 
-tweetsRouter.put("/:num", tweetController.updateTweet);
+tweetsRouter.put("/:num", validator, tweetController.updateTweet);
 
 tweetsRouter.delete("/:num", tweetController.deleteTweet);
 
